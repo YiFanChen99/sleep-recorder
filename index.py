717677d5089b8a518b80fabc3788d43b748dce8f
Ricky <pig211@gmail.com>
2014-05-20 15:21:24 +0800

    加入 gitignore 調整 GeneralInput.html 使其有送出表單的能力 將 index.py 與 recorder.py 的架構建好 使 index.py 能夠根據表單的輸入顯示內容

--- index.py
+++ index.py
@@ -1,22 +1,41 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
+from recorder import Recorder
+
 app = Flask(__name__)
 app.debug = True
+
 
 @app.route('/')
 def default():
     return redirect(url_for('index'))
 
+
 @app.route('/index')
 def index():
-	return render_template('index.html')
-    
-@app.route('/GeneralInput/')
-def general_input():
+    return render_template('index.html')
+
+
+@app.route('/GeneralInput/', methods=['POST'])
+def general_input_post():
+    if request.method == 'POST':
+        message = ""
+        message += "date=" + request.form['date'] + "\n"
+        message += "start_sleeping=" + request.form['start_sleeping'] + "\n"
+        message += "hours_on_sleep=" + request.form['hours_on_sleep'] + "\n"
+        message += "minutes_on_sleep=" + request.form['minutes_on_sleep'] + "\n"
+        message += "adjust_minute=" + request.form['adjust_minute'] + "\n"
+        return message
+
+
+@app.route('/GeneralInput/', methods=['GET'])
+def general_input_get():
     return render_template('GeneralInput.html')
 
+
 if __name__ == "__main__":
+    recorder = Recorder()
     app.run(host="0.0.0.0", port=5000)
-    
+
 '''
 return "<hi>Hellow you!!! <a href='#'  onClick='history.back()'>Ni bou hou</a>"
 '''
---
+++ recorder.py
@@ -0,0 +1,6 @@
+class Recorder(object):
+    def __init__(self):
+        self.counter = 0
+
+    def add(self):
+        self.counter += 1
